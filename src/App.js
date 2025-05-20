15b65527938309f7088f4e5287db3d1bfcb06e51
savi-creates <savannahandresson@gmail.com>
2025-05-20 14:40:40 +0100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Forecast App</div>;
+  return (
+    <div className="App">
+      <h1>Weather Forecast App</h1>
+      <footer>
+        Coded by{" "}
+        <a
+          href="#https://github.com/savi-creates"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Savannah Andresson {""}
+        </a>
+        and is {""}
+        <a
+          href="https://github.com/savi-creates/weather-forecast-app-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
