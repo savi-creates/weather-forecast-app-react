1b4aeae0bca626cb2e7568a3a773cce0beb7052d
savi-creates <savannahandresson@gmail.com>
2025-05-26 11:15:36 +0100

    Created new component and tracked the current unit

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
---
+++ src/WeatherTemperature.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+export default function WeatherTemperature(props) {
+  const [unit, setUnit] = useState("celsius");
+  function showFahrenheit(event) {
+    event.preventDefault();
+    setUnit("fahrenheit");
+  }
+
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
+  function fahrenheit() {
+    return (props.celsius * 9) / 5 + 32;
+  }
+
+  if (unit === "celsius") {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temp-number">{Math.round(props.celsius)}</span>
+        <span className="temp-unit">
+          {" "}
+          ºC |{" "}
+          <a href="/" onClick={showFahrenheit}>
+            ºF
+          </a>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="WeatherTemperature">
+        <span className="temp-number">{Math.round(fahrenheit())}</span>
+        <span className="temp-unit">
+          {" "}
+          <a href="/" onClick={showCelsius}>
+            ºC{" "}
+          </a>{" "}
+          | ºF
+        </span>
+      </div>
+    );
+  }
+}
